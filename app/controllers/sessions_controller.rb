04802d7b1fa6dc3eebe4c2f3b2ff544c3b8694c7
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-12 10:57:11 +0300

    Backend:Added session routes and actions layout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    #POST /login
+    def create
+    end
+
+    #GET /me
+    def show
+    end
+
+    #DELETE /logout
+    def destroy
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :loans
   resources :users, only:[:create,:show]
   resources  :loan_applications,only:[:index,:create,:show]
+        #Session routes
+  post "/login",to "sessions#create"
+  get "/me", to "sessions#show"
+  delete "/logout", to "sessions#destroy" 
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
