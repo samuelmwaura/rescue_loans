569c5235f1d402a0cf548980bf057798f635b4f3
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-12 10:06:27 +0300

    Backend:Added the model associations

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,4 @@
 class Loan < ApplicationRecord
+    has_many :loan_applications
+    has_many :users, through: :loan_applications
 end
--- app/models/loan_application.rb
+++ app/models/loan_application.rb
@@ -1,2 +1,4 @@
 class LoanApplication < ApplicationRecord
+    belongs_to :loan
+    belongs_to :user
 end
